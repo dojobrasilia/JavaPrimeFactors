60bce2c98c624e1da8fd4b6acc547f799fe52174
Fabricio Buzeto <nuk.anime13@gmail.com>
2012-03-14 18:46:27 -0300

    fim do dojo =P

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -1,8 +1,59 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 
 public class PrimeFactor {
 
-	public static int[][] factor(int i) {
-		return new int[][]{{i,1}};
+	private static Set<Integer> primes = new HashSet<Integer>();
+	
+	public static int[][] factor(int n) {
+		
+		List<int[]> factorsList = new ArrayList<int[]>();
+		
+		int i = 2;
+		do {
+			
+			if (n % i == 0){
+				int cont = factorsOf(n,i);
+				factorsList.add(new int[]{i, cont});
+			}
+			
+			i = nextPrime(i);
+			
+		} while(i<=n);
+		
+		return factorsList.toArray(new int[0][0]);
 	}
 
+	public static int nextPrime(int i) {
+		do{
+			i++;
+		} while(!isPrime(i));
+		return i;
+	}
+
+	private static int factorsOf(int n, int factor) {
+		int temp=n;
+		int cont = 0;
+		while (temp % factor == 0){
+			cont++;
+			temp /= factor;
+		}
+		return cont;
+	}
+
+	public static boolean isPrime(int number) {
+		if (primes.contains(number)) return true;
+		
+		for(int i=2; i<=number/2 ; i++){
+			if (number % i == 0){
+				return false;
+			}
+		}
+		primes.add(number);
+		return true;
+	}
+	
 }
--- test/_PrimeFactorTest.java
+++ test/_PrimeFactorTest.java
@@ -10,4 +10,32 @@ public class _PrimeFactorTest {
 		assertEquals(new int[][]{{3,1}},PrimeFactor.factor(3));
 	}
 	
+	@Test public void calculateFactorsOfTheSameNumber(){
+		assertEquals(new int[][]{{2,2}},PrimeFactor.factor(4));
+		assertEquals(new int[][]{{2,3}},PrimeFactor.factor(8));
+		assertEquals(new int[][]{{2,5}},PrimeFactor.factor(32));
+		assertEquals(new int[][]{{3,2}},PrimeFactor.factor(9));
+		assertEquals(new int[][]{{5,5}},PrimeFactor.factor((int)Math.pow(5,10)));
+	}
+
+	@Test public void calculateFactorsOfTwoPrimes(){
+		assertEquals(new int[][]{{2,2},{3,2}},PrimeFactor.factor((int)Math.pow(2,2)*(int)Math.pow(3,2)));
+		assertEquals(new int[][]{{2,2},{5,2}},PrimeFactor.factor(100));
+		assertEquals(new int[][]{{2,1},{3,1},{5,1}},PrimeFactor.factor(30));
+	}
+	
+	@Test public void checkIfANumberIsPrime(){
+		assertEquals(true, PrimeFactor.isPrime(2));
+		assertEquals(true, PrimeFactor.isPrime(3));
+		assertEquals(true, PrimeFactor.isPrime(17));
+		assertEquals(false, PrimeFactor.isPrime(15));
+		assertEquals(false, PrimeFactor.isPrime(4));
+	}
+	
+	@Test public void calculateNextPrime(){
+		assertEquals(3, PrimeFactor.nextPrime(2));
+		assertEquals(5, PrimeFactor.nextPrime(3));
+		assertEquals(17, PrimeFactor.nextPrime(13));
+	}
+	
 }
